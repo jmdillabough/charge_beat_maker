56df3a37c7f3e9a02e4b9c2218e4c7ae82d4427d
Jonathan Dillabough <JDillabough87@gmail.com>
2022-04-13 12:36:07 -0500

    completed

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-window.addEventListener('keydown', (e) => {
-	console.log(e)
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+	key.classList.add('playing')
+
+	if (!audio) return
+	audio.currentTime = 0
+	audio.play()
+}
+
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return
+	if (e.propertyName == 'transform') this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach((key) => {
+	key.addEventListener('transitionend', removeTransition)
 })
+
+window.addEventListener('keydown', playSound)
